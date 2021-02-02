298e39d25c4b36acf3031aa2f2c4b94547bf102c
DESKTOP-BGEFQLI\Administrator <rkdvnfma90@hanmail.net>
2021-02-02 10:06:41 +0900

    add useTabs hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useInput } from './hooks/useInput.js'
+import { useTabs } from './hooks/useTabs.js'
 import './App.css'
 
 const App = () => {
@@ -18,13 +19,25 @@ const App = () => {
       content: '섹션 2 입니다.',
     },
   ]
+  const { currentItem, changeItem } = useTabs(0, content)
 
   return (
     <div className="app-main">
       <h1 className="app-title">React Simple Hooks</h1>
+
       <section className="app-hooks">
         <h3 className="app-desc">useInput : </h3>
         <input placeholder="Name" {...name} />
+      </section>
+
+      <section className="app-hooks">
+        <h3 className="app-desc">useTab : </h3>
+        {content.map((section, index) => (
+          <button key={index} onClick={() => changeItem(index)}>
+            {section.tab}
+          </button>
+        ))}
+        {currentItem.content}
       </section>
     </div>
   )
